dbc35d078bf5ad8f56cc13fec6200dabcdec0b0f
evellyne5 <149822211+evellyne5@users.noreply.github.com>
2024-05-26 10:37:48 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,17 +3,19 @@ const allowedCors = [
 	"https://students-projects.ru",
 	"https://localhost:3000",
 	"https://localhost:3001",
-	"mongodb://localhost:27017"
+	"mongodb://localhost:27017",
+	"https://pindiefrontyandex.nomoredomainswork.ru"
 ]
 
 function cors(req, res, next) {
-	const {origin} = req.headers;
-	if(allowedCors.includes(origin)) {
-		res.hesder("Access-Control-Allow-Origin", origin);
-		res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-		res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-	}
-	next();
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
 }
 
 module.exports = cors;
